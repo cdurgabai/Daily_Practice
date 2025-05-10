093b07d5395fcf684ac12cd7ea3c425e65f5e9be
C Durga Bai <cdurgabai26@gmail.com>
2025-05-10 22:26:15 +0530

    Find Min

---
+++ findMin.py
@@ -0,0 +1,17 @@
+def findMin(arr):
+    low = 0
+    high = len(arr)-1
+    Min = float("inf")
+    while(low<=high):
+        mid=(low+high)//2
+        if arr[low]<=arr[mid]:
+            if arr[low]<=Min:
+                Min=arr[low]
+            low=mid+1
+        elif arr[mid]<=arr[high]:
+            if arr[mid]<=Min:
+                Min=arr[mid]
+            high=mid-1
+    return Min
+arr = list(map(int,input().split()))
+print(findMin(arr))
