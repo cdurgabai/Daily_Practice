9c24dd1d79cfdb8934490c1e28303a76031238dd
C Durga Bai <cdurgabai26@gmail.com>
2025-05-19 18:54:13 +0530

    SelectionSort.py

---
+++ SelectionSort.py
@@ -0,0 +1,8 @@
+def selectionSort(arr,n):
+    for i in range(0,n):
+        Min=i 
+        for j in range(i+1,n):
+            if arr[j]<arr[Min]:
+                Min=j
+        arr[i],arr[Min]=arr[Min],arr[i]
+    return arr
