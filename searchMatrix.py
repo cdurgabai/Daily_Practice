d64d6222996647b894000926de768a96d76ce260
C Durga Bai <cdurgabai26@gmail.com>
2025-07-21 15:42:22 +0530

    search Matrix

---
+++ searchMatrix.py
@@ -0,0 +1,16 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        if not matrix:
+            return False
+        
+        m, n = len(matrix), len(matrix[0])
+        i, j = 0, n - 1
+        while i < m and j >= 0:
+            if matrix[i][j] < target:
+                i += 1
+            elif matrix[i][j] > target:
+                j -= 1
+            else:
+                return True
+
+        return False 
