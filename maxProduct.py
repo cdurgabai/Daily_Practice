af243cd1015de05c3aaceddf5e3e8f1c6f413b1c
C Durga Bai <cdurgabai26@gmail.com>
2025-08-08 21:14:44 +0530

    Create maxProduct

---
+++ maxProduct.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxProduct(self, words: List[str]) -> int:
+        res = float('-inf')
+        l, r = 0, 1
+        while l < len(words) - 1:
+            set1 = set(words[l])
+            set2 = set(words[r])
+            if not set1 & set2:
+                res = max(res, len(words[l]) * len(words[r]))
+            r += 1
+            if r == len(words):
+                l += 1
+                r = l + 1
+        return res if res != float('-inf') else 0
