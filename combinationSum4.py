18c84a240a178251a17d0ddfdc446bc6b8212dd4
C Durga Bai <cdurgabai26@gmail.com>
2025-08-12 19:11:20 +0530

    Create combinationSum4

---
+++ combinationSum4.py
@@ -0,0 +1,15 @@
+class Solution:
+    def combinationSum4(self, nums, target):
+        memo = {}
+        def dfs(remain):
+            if remain == 0:
+                return 1
+            if remain in memo:
+                return memo[remain]
+            count = 0
+            for num in nums:
+                if num <= remain:
+                    count += dfs(remain - num)
+            memo[remain] = count
+            return count
+        return dfs(target)
